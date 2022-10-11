93f57ddba4cfa77ad2ac43fb769f4a600eb34845
zohaib1313 <mianzohaibifikhar@gmail.com>
2022-10-11 23:53:15 +0500

    ''

--- zohaib_portfolio/flutter_service_worker.js
+++ zohaib_portfolio/flutter_service_worker.js
@@ -25,8 +25,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "953259acc3f43250d7b7f048d32ada2e",
-"/": "953259acc3f43250d7b7f048d32ada2e",
+"index.html": "959d6a625d438d45781207c9f4c69b32",
+"/": "959d6a625d438d45781207c9f4c69b32",
 "main.dart.js": "155a6898b74970fe5dba2b0e0b9e9674",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
